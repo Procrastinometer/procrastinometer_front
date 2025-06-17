f9ac49a89d6bfa064b8afef1d346799e517d3ff9
Antonio <antony.dovzhenko@gmail.com>
2025-06-17 15:05:49 +0300

    Update routes

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,22 @@
   import NotFound from './routes/NotFound.svelte';
   import Projects from "./routes/Projects.svelte";
   import Day from "./routes/Day.svelte";
+  import IDE from "./routes/IDE.svelte";
+  import OS from "./routes/OS.svelte";
+  import Year from "./routes/Year.svelte";
+  import Settings from "./routes/Settings.svelte";
+  import Week from "./routes/Week.svelte";
 
   const routes = {
     '/': Dashboard,
     '/dashboard': Dashboard,
     '/projects': Projects,
     '/day': Day,
+    '/week': Week,
+    '/ide': IDE,
+    '/os': OS,
+    '/year': Year,
+    '/settings': Settings,
 
     '*': NotFound,
   };
